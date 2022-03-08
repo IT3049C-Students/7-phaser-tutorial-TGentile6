426be628eebfc64dd85933e21001957904b05131
TGentile6 <91705356+TGentile6@users.noreply.github.com>
2022-03-08 14:59:03 -0500

    updates game files

--- game.js
+++ game.js
@@ -1,3 +1,26 @@
 window.onload = function(){
-    var game = new Phaser.Game();
+    var game = new Phaser.Game(config);
 }
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 }
+        }
+    },
+    scene: {
+        preload,
+        create,
+        update
+    }
+}
+
+function preload(){}
+
+function create(){}
+
+function update(){}
