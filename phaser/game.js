3cf7e5168a5d36b31da58863da3b89033ef5b2bf
TGentile6 <91705356+TGentile6@users.noreply.github.com>
2022-03-23 14:27:34 -0400

    completed first 2 parts

--- phaser/game.js
+++ phaser/game.js
@@ -6,6 +6,8 @@ const config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    backgroundColor: 0x000000,
+    scene: [scene1, scene2],
     physics: {
         default: 'arcade',
         arcade: {
